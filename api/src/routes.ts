c41e799f0c42647ad576f0dec25b08710d7b59d0
Allan Gomes <allan@niu.net.br>
2023-11-20 16:40:18 -0300

    token

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,23 +1,39 @@
-import  express from 'express';
+import  express, { NextFunction, response } from 'express';
 const routes = express.Router(); 
 const fs = require("fs");
 const path = require('path');
+import "express-async-errors";
+
+
+// const authMiddleware = require('./middlewares/auth') 
 
 import {Produto} from './domain/entities/produto';
 // import {FileExport} from './domain/entities/fileexport';
 // import {Ordens} from './domain/entities/ordens';
 import { Administrador } from './domain/entities/administardor';
+import { Usuario } from './domain/entities/usuario';
+import { authAdministrador } from './middlewares/authAdministrador';
+import { Etiqueta } from './domain/entities/etiqueta';
 
 var prefix = '';
 
-prefix = '/administrador';
-routes.post(`${prefix}`, Administrador.cadastro);
+prefix = '/usuario';
+routes.post(`${prefix}/login`, Usuario.login);
 
-prefix = '/product';
-routes.post(`${prefix}/cadastar`,Produto.cadastrar);
-routes.put(`${prefix}/alterar/:id`,Produto.alterar);
-routes.get(`${prefix}/buscar-por-cod`,Produto.buscarPorCod);
-routes.get(`${prefix}/buscar-ativo-por-cod`,Produto.buscarAtivoPorCod);
+prefix = '/administrador';
+routes.post(`${prefix}/cadastrar`, Administrador.cadastrar);
+routes.put(`${prefix}/alterar/:id`, Administrador.alterar);
+
+prefix = '/produto';
+routes.post(`${prefix}/cadastrar`,authAdministrador,Produto.cadastrar);
+routes.put(`${prefix}/alterar/:id`,authAdministrador,Produto.alterar);
+routes.get(`${prefix}/buscar-por-cod`,authAdministrador,Produto.buscarPorCod);
+routes.get(`${prefix}/buscar-ativo-por-cod`,authAdministrador,Produto.buscarAtivoPorCod);
+
+prefix = '/etiqueta';
+routes.post(`${prefix}/cadastrar`,authAdministrador,Etiqueta.cadastrar);
+routes.put(`${prefix}/alterar/:id`,authAdministrador,Etiqueta.alterar);
+routes.get(`${prefix}/buscar-ativo-por-cod`,authAdministrador,Produto.buscarAtivoPorCod);
 
 // prefix = '/file';
 // routes.get(`${prefix}/generate/:cod/:lote/:etiqueta/:qtd`, FileExport.file2);
@@ -26,7 +42,6 @@ routes.get(`${prefix}/buscar-ativo-por-cod`,Produto.buscarAtivoPorCod);
 // prefix = '/production';
 // routes.get(`${prefix}/listOrder`, Ordens.listOrdens);
 // routes.delete(`${prefix}/delete`, Ordens.delete);
-
 
 
 module.exports = routes    
