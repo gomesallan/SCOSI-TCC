7041264d0d007887de105f7174777437bf50de98
Allan Gomes <allan@niu.net.br>
2023-11-21 18:24:39 -0300

    front: base schadcn

---
+++ front-end/src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <div className="flex min-h-screen bg-slate-50 items-center justify-center">
+        <h1>Teste</h1>
+      </div>
+    )
+  }
--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,7 +1,55 @@
-import Image from 'next/image'
+import * as React from "react"
+ 
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export default function Home() {
   return (
-    <h1>Teste</h1>
+    <div className="flex min-h-screen bg-slate-50 items-center justify-center">
+      <CardLogin/>
+    </div>
+  )
+}
+export function CardLogin() {
+  return (
+    <Card className="w-[550px]">
+      <CardHeader>
+        <CardTitle>Entrar</CardTitle>
+        <CardDescription>Informe seu usuario e senha.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Usuario</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="senha">Senha</Label>
+              <Input type="password" id="senha" placeholder="Name of your project" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button>Entrar</Button>
+      </CardFooter>
+    </Card>
   )
 }
